0d8eb69ca12e96be0d218c42d2964b80eb591162
Kaverappa KU <caakash357@gmail.com>
2023-10-18 20:07:26 -0400

    adding assignment 2 contents

---
+++ N01589651_Assignment2/Assignment2/Controllers/J2Controller.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Xml.Schema;
+
+namespace Assignment2.Controllers
+{
+    public class J2Controller : ApiController
+    {
+        /// <summary>
+        /// To get the shifted sum of a number. Shifting a number means adding a 0 to the number, and add that to the original number.
+        /// For eg: 12 is the number, after shifting it will be 120, so 120 + 12 will give you 132.
+        /// </summary>
+        /// <param name="N">Denotes the number to be shifted, this should be a positive integer between 1 and 10000</param>
+        /// <param name="k">Denotes the number of times N needs to be shifted, this should be a positive integer between 0 and 5</param>
+        /// <returns>
+        /// GET api/j2/shiftysum/12/1 -> Shifty Sum: 132
+        /// GET api/j2/shiftysum/12/3 -> Shifty Sum: 13332
+        /// GET api/j2/shiftysum/12/6 -> Input proper values
+        /// GET /api/j2/shiftysum/12000/2 -> Input proper values
+        /// </returns>
+
+        [HttpGet]
+        [Route("api/J2/ShiftySum/{N}/{k}")]
+        public string ShiftySum(int N, int k)
+        {
+            double shiftySum = 0;
+            shiftySum += N;
+            if (N>=1 && N<=10000 && k>=0 && k <= 5)
+            {
+                for (double i = 1; i<=k; i++)
+                {
+                    shiftySum += N * Math.Pow(10, i);
+                }
+                return "Shifty Sum: " + shiftySum;
+            }
+            return "Input proper values";
+        }
+    }
+}
---
+++ N01589651_Assignment2/Assignment2/Controllers/J3Controller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment2.Controllers
+{
+    public class J3Controller : ApiController
+    {
+        /// <summary>
+        /// Gives the Sumac sequence of the two numbers entered.The difference between two entered numbers will be 
+        /// the input for the next calculation, so t2 will become the minuend and t3 will become the subtahend.
+        /// This will be carried until t2 becomes negative.The total number of calculation possible we get a 
+        /// negative number gives the Sumac sequence.
+        /// For eg: 120 and 71, the Sumac sequence will be 120, 71, 49, 22, 27. So the length of Sumac sequence is 5.
+        /// </summary>
+        /// <param name="t1">Denotes the first value</param>
+        /// <param name="t2">Denotes the second value</param>
+        /// <returns>
+        /// GET /api/j3/sumacsequence/120/71 -> Length of Sumac sequence: 5
+        /// GET /api/j3/sumacsequence/30/40 -> Length of Sumac sequence: 2
+        /// GET /api/j3/sumacsequence/100/-1 -> Input proper values
+        /// GET /api/j3/sumacsequence/52000/2000 -> Input proper values
+        /// </returns>
+
+        [HttpGet]
+        [Route("api/J3/SumacSequence/{t1}/{t2}")]
+        public string SumacSequence(int t1, int t2)
+        {
+            int temp;
+            int sequence = 1;
+            if (t1 < 10000 && t2 > 0)
+            {
+                while (t2 >= 0)
+                {
+                    temp = t1;
+                    t1 = t2;
+                    t2 = temp - t2;
+                    sequence++;
+                }
+                return "Length of Sumac sequence: " + sequence;
+            }
+            return "Input proper values";
+        }
+    }
+}
