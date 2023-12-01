100cca911c8a0b2dc65cf97536c9879947f660b8
Kaverappa KU <caakash357@gmail.com>
2023-12-01 16:33:24 -0500

    Adding Cumulative Project 2

--- N01589651_CumulativeProject1/CumulativeProject/Controllers/TeacherController.cs
+++ N01589651_CumulativeProject1/CumulativeProject/Controllers/TeacherController.cs
@@ -14,11 +14,12 @@ namespace CumulativeProject.Controllers
         {
             return View();
         }
-        //GET : /Teacher/List
-        public ActionResult ListTeachers()
+
+        //GET : /Teacher/ListTeachers/{SearchKey?}
+        public ActionResult ListTeachers(string SearchKey = null)
         {
             TeacherDataController controller = new TeacherDataController();
-            IEnumerable<Teacher> Teachers = controller.ListTeachers();
+            IEnumerable<Teacher> Teachers = controller.ListTeachers(SearchKey);
             return View(Teachers);
         }
 
@@ -30,5 +31,54 @@ namespace CumulativeProject.Controllers
             return View(NewTeacher);
         }
 
+        // Function which goes to Teacher View 
+        // Views/Teacher/New.cshtml
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        // Function to add new teachers to database
+        // Adds new teacher details from the form and returns to list teachers 
+        [HttpPost]
+        public ActionResult Add(string TeacherFname, string TeacherLname, string EmployeeNumber, DateTime HireDate, Decimal Salary) { 
+
+            // Creating teacher objects and assigning parameters to that object
+            Teacher NewTeacher = new Teacher();
+
+            NewTeacher.TeacherFname = TeacherFname;
+            NewTeacher.TeacherLname = TeacherLname;
+            NewTeacher.EmployeeNumber = EmployeeNumber;
+            NewTeacher.HireDate = HireDate;
+            NewTeacher.Salary = Salary;
+
+            //Creating an object for teacher data controller inorder to add the new data
+            TeacherDataController controller = new TeacherDataController();
+
+            // Adding the record 
+            controller.AddTeacher(NewTeacher);
+
+            // returns to the list teachers page back
+            return RedirectToAction("ListTeachers");
+        }
+
+        //POST : /Teacher/Delete/{id}
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            controller.DeleteTeacher(id);
+            return RedirectToAction("ListTeachers");
+        }
+
+        //GET : /Teacher/DeleteConfirm/{id}
+        public ActionResult DeleteConfirm(int id)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            Teacher NewTeacher = controller.FindTeacher(id);
+
+
+            return View(NewTeacher);
+        }
     }
 }
